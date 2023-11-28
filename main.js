8e7cba0deb66c36473ba0787c2eec748aa7ea2ac
Baxtiyar Azimov <baxtiyar.azimov@aahm.lan>
2023-11-28 11:52:37 +0400

    add

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+const form = document.querySelector(".form");
+const btn = document.querySelector('.btn');
+const icon = document.querySelector(".icon");
+const form_box_btn_1 = document.querySelector(".form_box_btn_1");
+const form_box_btn_2 = document.querySelector(".form_box_btn_2");
+
+btn.addEventListener('click',function (){
+    form.classList.toggle('active')
+})
+
+icon.addEventListener('click',function (){
+    form.classList.remove('active')
+})
+
+form_box_btn_1.addEventListener('click',function (){
+    form.classList.remove('active')
+})
+
+form_box_btn_2.addEventListener('click',function (){
+    form.classList.remove('active')
+    
+})

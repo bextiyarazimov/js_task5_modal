c31f378524c2bb6e2868613d957e41419714ae9a
bextiyar <azimovbextiyar1989@gmail.com>
2023-11-29 18:55:51 +0400

    add

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const btn = document.querySelector('.btn');
 const icon = document.querySelector(".icon");
 const form_box_btn_1 = document.querySelector(".form_box_btn_1");
 const form_box_btn_2 = document.querySelector(".form_box_btn_2");
+const test =document.querySelector("#test");
 
 btn.addEventListener('click',function (){
     form.classList.toggle('active')
@@ -20,5 +21,5 @@ form_box_btn_1.addEventListener('click',function (){
 
 form_box_btn_2.addEventListener('click',function (){
     form.classList.remove('active')
-    
+   alert(test.value);
 })
